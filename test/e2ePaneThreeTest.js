356a3216290cf13c00f278d24e7a158203092a79
sdtsui <danielt213@gmail.com>
2015-02-23 19:45:35 -0800

    feat: additional testing file

---
+++ test/e2ePaneThreeTest.js
@@ -0,0 +1,53 @@
+var expect = require('chai').expect;
+var superagent = require('superagent');
+
+describe('', function(){
+
+  var testUserID = 'testUser';
+
+
+  describe('can book a time', function(){
+    it('searches for all available times for a course', function(){
+
+    });
+
+    xit('books a specific time', function(){
+
+    });
+
+    xit('searches for that time, and checks that it is booked', function(){
+
+    });
+  });
+
+
+  xdescribe('', function(){
+    it('', function(){
+
+    });
+
+    it('', function(){
+
+    });
+
+    it('', function(){
+
+    });
+  })
+
+  xdescribe('', function(){
+    it('', function(){
+
+    });
+
+    it('', function(){
+
+    });
+
+    it('', function(){
+
+    });
+  })
+
+
+});
